2c979930b200c3d973289b078c96a9a86d7a98e7
Marco Callado <47508053+callado5@users.noreply.github.com>
2023-03-30 15:44:47 -0300

    Update FirstFunction.f95

--- FirstFunction.f95
+++ FirstFunction.f95
@@ -7,17 +7,18 @@ program teste
     write(*,*) 'x:'
     read(*,*) x
     
-    y =expcos(x)
+    y =expcos(x) !using the function bellow
     
     write(*,*) 'Valor da funcao:', y
     
-    Contains
+    Contains !Allows me to use the function bellow 
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    Real function expcos(x)
+    Real function expcos(x) 
     
-        Real, intent(in)::x
-        expcos = exp(-x**2)*cos(10*x)
+        Real, intent(in)::x !here I'm inserting the input varibles of the function
+        expcos = exp(-x**2)*cos(10*x)!declaring the function
     
     end function expcos
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!    
 end program teste
